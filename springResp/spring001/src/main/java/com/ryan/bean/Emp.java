132f5e27466ad486268b3568590190503655229d
ryan <807937928@qq.com>
2023-04-09 17:29:53 +0800

    自动装配/注入-xml、bean标签配置模式、autowire

---
+++ springResp/spring001/src/main/java/com/ryan/bean/Emp.java
@@ -0,0 +1,13 @@
+package com.ryan.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emp {
+    private Dept dept;
+
+}
--- springResp/spring001/src/test/java/com/ryan/EmpDaoTest.java
+++ springResp/spring001/src/test/java/com/ryan/EmpDaoTest.java
@@ -1,16 +1,17 @@
 package com.ryan;
 
-import com.ryan.bean.Emp;
 import com.ryan.dao.EmpDao;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class EmpTest {
+public class EmpDaoTest {
     @Test
     public void testBean() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring6.xml");
-        Emp o1 = context.getBean("emp", Emp.class);
-        System.out.println(o1);
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        EmpDao empDao = context.getBean("empDao", EmpDao.class);
+        empDao.addEmp();
     }
+
+
 }
