2e992585a77f35210b8bd62c4a114c2587254c11
ryan <807937928@qq.com>
2023-04-10 15:59:38 +0800

    注解方式创建对象IOC-组件扫描配置注解识别

--- springResp/spring002/src/main/java/com/ryan/bean/User.java
+++ springResp/spring002/src/main/java/com/ryan/bean/User.java
@@ -1,9 +1,15 @@
 package com.ryan.bean;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Component
+//@Component
 //@Component(value = "user1")
+@Controller
+//@Service
+//@Repository
 public class User {
 
 }
